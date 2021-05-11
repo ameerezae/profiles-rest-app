7bc4f59773873aacc085b5e6d57ba1d026eae229
amirrezaeii <amir.rezaei1999@yahoo.com>
2021-05-11 23:46:30 +0430

    Add user profile and user manager models

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError('You must specify email address.')
+
+        email = self.normalize_email(email)
+        user = self.model(name=name, email=email)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
 
 # Create your models here.
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
