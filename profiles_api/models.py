a67373ca1f0a42f8fa3702daa8b42df6c31f518e
amirrezaeii <amir.rezaei1999@yahoo.com>
2021-05-27 15:28:27 +0430

    Edit user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -3,19 +3,18 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 
 class UserProfileManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
-        if not email:
-            raise ValueError('You must specify email address.')
+    def create_user(self, username, name, age, password=None):
+        if not username:
+            raise ValueError('You must specify username.')
 
-        email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(username=username, name=name, age=age)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email, name, password):
-        user = self.create_user(email, name, password)
+    def create_superuser(self, username, name, age, password):
+        user = self.create_user(username, name, age, password)
         user.is_superuser = True
         user.is_staff = True
 
@@ -24,18 +23,19 @@ class UserProfileManager(BaseUserManager):
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=255)
+    age = models.IntegerField(default=None)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['name', 'age']
 
     def get_name(self):
         return self.name
 
     def __str__(self):
-        return self.email
+        return self.username
