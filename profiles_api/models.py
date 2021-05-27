cee6942a675d59577a64ff7a7042233113b71afc
amirrezaeii <amir.rezaei1999@yahoo.com>
2021-05-27 13:14:46 +0430

    Edit user profile class

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -8,20 +8,28 @@ class UserProfileManager(BaseUserManager):
             raise ValueError('You must specify email address.')
 
         email = self.normalize_email(email)
-        user = self.model(name=name, email=email)
+        user = self.model(email=email, name=name)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
 
-# Create your models here.
+        user.save(using=self._db)
+        return user
+
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
